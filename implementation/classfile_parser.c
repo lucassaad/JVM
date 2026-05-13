cca076b16a5a4873190bed489495a59e08349c9b
Lucas Saad Rodrigues <lucassaadro@gmail.com>
2026-05-13 17:51:22 -0300

    constant_pool.h and switch to build structure

--- implementation/classfile_parser.c
+++ implementation/classfile_parser.c
@@ -2,47 +2,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <arpa/inet.h>
+#include "constant_pool.h"
 
 
-typedef enum cp_tags {
-    CONSTANT_Utf8 = 1,
-    CONSTANT_Integer = 3,
-    CONSTANT_Flaot = 4,
-    CONSTANT_Long = 5,
-    CONSTANT_Double = 6,
-    CONSTANT_Class = 7,
-    CONSTANT_String = 8,
-    CONSTANT_Fieldred = 9,
-    CONSTANT_Methodref = 10,
-    CONSTANT_InterfaceMethodref = 11,
-    CONSTANT_NameAndType = 12, 
-    CONSTANT_MethodHandle = 15,
-    CONSTANT_MethodType = 16,
-    CONSTANT_Dynamic = 17,
-    CONSTANT_InvokeDynamic = 18,
-    CONSTANT_Module = 19,
-    CONSTANT_Package = 20
-} cp_tags;
-
-
-// CONSTANT_Class_info
-// CONSTANT_Fieldref_info
-// CONSTANT_Methodref_info
-// CONSTANT_InterfaceMethodref_info
-// CONSTANT_String_info
-// CONSTANT_Integer_info
-// CONSTANT_Float_info
-// CONSTANT_Long_info
-// CONSTANT_Double_info
-// CONSTANT_NameAndType_info
-// CONSTANT_Utf8_info
-
-// CONSTANT_MethodHandle_info
-// CONSTANT_MethodType_info
-// CONSTANT_Dynamic_info
-// CONSTANT_InvokeDynamic_info
-// CONSTANT_Module_info
-// CONSTANT_Package_info
 
 // Constant Pool entry format
 typedef struct {
@@ -110,11 +72,11 @@ int main() {
     printf("constant_pool_count: 0x%04X\n", cf->constant_pool_count);
     
     
-    for (int i = 0; i < constant_pool_count; i++) {
+    for (int i = 0; i < cf->constant_pool_count; i++) {
         uint8_t tag;
         if (fread(&tag, sizeof(tag), 1, arquivo) != 1) {
             perror("Erro na leitura da tag");
-            return 1
+            return 1;
         }
         // mapear tag para a informação
         // alocar memoria para o tipo da informacao 
@@ -130,4 +92,276 @@ int main() {
 
 
     return 0;
+}
+
+
+void* constant_pool_reader(cp_tags tag) {
+    switch (tag) {
+        case CONSTANT_Class: {
+                    
+            CONSTANT_Class_info *entry = malloc(sizeof(CONSTANT_Class_info));
+            entry->tag = tag;
+
+            if (fread(&entry->name_index, sizeof(entry->name_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return;
+            }
+            entry->name_index = ntohs(entry->name_index);
+
+            return entry;
+        }
+
+        case CONSTANT_Fieldref: {
+            CONSTANT_Fieldref_info *entry = malloc(sizeof(CONSTANT_Fieldref_info));
+            entry->tag = tag;
+            
+            // Read 'class_index':
+            if (fread(&entry->class_index, sizeof(entry->class_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->class_index = ntohs(entry->class_index);
+            
+            // Read name_and_type_index:
+            if (fread(&entry->name_and_type_index, sizeof(entry->name_and_type_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->name_and_type_index = ntohs(entry->name_and_type_index);
+
+            return entry;    
+        }
+
+        case CONSTANT_Methodref: {
+            CONSTANT_Methodref_info *entry = malloc(sizeof(CONSTANT_Methodref_info));
+            entry->tag = tag;
+            
+            // Read 'class_index':
+            if (fread(&entry->class_index, sizeof(entry->class_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->class_index = ntohs(entry->class_index);
+            
+            // Read name_and_type_index:
+            if (fread(&entry->name_and_type_index, sizeof(entry->name_and_type_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->name_and_type_index = ntohs(entry->name_and_type_index);
+
+            return entry;    
+        }
+        
+        case CONSTANT_InterfaceMethodref: {
+            CONSTANT_InterfaceMethodref_info *entry = malloc(sizeof(CONSTANT_InterfaceMethodref_info));
+            entry->tag = tag;
+            
+            // Read 'class_index':
+            if (fread(&entry->class_index, sizeof(entry->class_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->class_index = ntohs(entry->class_index);
+            
+            // Read name_and_type_index:
+            if (fread(&entry->name_and_type_index, sizeof(entry->name_and_type_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->name_and_type_index = ntohs(entry->name_and_type_index);
+    
+            return entry;    
+        }
+        
+        case CONSTANT_String: {
+            CONSTANT_String_info *entry = malloc(sizeof(CONSTANT_String_info));
+            entry->tag = tag;
+            
+            // Read name_and_type_index:
+            if (fread(&entry->string_index, sizeof(entry->string_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->string_index = ntohs(entry->string_index);
+            
+            return entry;
+        }
+        
+        
+        case CONSTANT_Integer: {
+            CONSTANT_Integer_info *entry = malloc(sizeof(CONSTANT_Integer_info));
+            entry->tag = tag;
+            
+            // Read name_and_type_index:
+            if (fread(&entry->bytes, sizeof(entry->bytes), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->bytes = ntohl(entry->bytes);
+            
+            return entry;
+        }
+        
+        case CONSTANT_Float: {
+            CONSTANT_Float_info *entry = malloc(sizeof(CONSTANT_Float_info));
+            entry->tag = tag;
+            
+            // Read name_and_type_index:
+            if (fread(&entry->bytes, sizeof(entry->bytes), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->bytes = ntohl(entry->bytes);
+            
+            return entry;
+        }
+        
+        case CONSTANT_Long: {
+            CONSTANT_Long_info *entry = malloc(sizeof(CONSTANT_Long_info));
+            entry->tag = tag;
+            
+            // Read name_and_type_index:
+            if (fread(&entry->high_bytes, sizeof(entry->high_bytes), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->high_bytes = ntohl(entry->high_bytes);
+            
+            if (fread(&entry->low_bytes, sizeof(entry->low_bytes), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->low_bytes = ntohl(entry->low_bytes);
+
+            return entry;
+        }
+        
+        case CONSTANT_Double: {
+            CONSTANT_Double_info *entry = malloc(sizeof(CONSTANT_Double_info));
+            entry->tag = tag;
+            
+            // Read name_and_type_index:
+            if (fread(&entry->high_bytes, sizeof(entry->high_bytes), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->high_bytes = ntohl(entry->high_bytes);
+            
+            if (fread(&entry->low_bytes, sizeof(entry->low_bytes), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->low_bytes = ntohl(entry->low_bytes);
+            
+            return entry;
+        }
+
+        case CONSTANT_NameAndType: {
+            CONSTANT_NameAndType_info *entry = malloc(sizeof(CONSTANT_NameAndType_info));
+            entry->tag = tag;
+            
+            if (fread(&entry->name_index, sizeof(entry->name_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->name_index = ntohs(entry->name_index);
+            
+            if (fread(&entry->descriptor_index, sizeof(entry->descriptor_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->descriptor_index = ntohs(entry->descriptor_index);
+            
+            return entry;
+        }
+        
+        case CONSTANT_Utf8: {
+            break;
+        }
+        
+        case CONSTANT_MethodHandle: {
+            CONSTANT_MethodHandle_info *entry = malloc(sizeof(CONSTANT_MethodHandle_info));
+            entry->tag = tag;
+            
+            if (fread(&entry->reference_kind, sizeof(entry->reference_kind), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            
+            if (fread(&entry->reference_index, sizeof(entry->reference_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->reference_index = ntohs(entry->reference_index);
+
+            return entry;
+        }
+        
+        case CONSTANT_MethodType: {
+            CONSTANT_MethodType_info *entry = malloc(sizeof(CONSTANT_MethodType_info));
+            entry->tag = tag;
+            
+            if (fread(&entry->descriptor_index, sizeof(entry->descriptor_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->descriptor_index = ntohs(entry->descriptor_index);
+
+            return entry;
+        }
+        
+        case CONSTANT_Dynamic: {
+            CONSTANT_Dynamic_info *entry = malloc(sizeof(CONSTANT_Dynamic_info));
+            entry->tag = tag;
+            
+            if (fread(&entry->bootstrap_method_attr_index, sizeof(entry->bootstrap_method_attr_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->bootstrap_method_attr_index = ntohs(entry->bootstrap_method_attr_index);
+            
+            if (fread(&entry->name_and_type_index, sizeof(entry->name_and_type_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->name_and_type_index = ntohs(entry->name_and_type_index);
+
+            return entry;
+        }
+
+        case CONSTANT_InvokeDynamic: {
+            CONSTANT_InvokeDynamic_info *entry = malloc(sizeof(CONSTANT_InvokeDynamic_info));
+            entry->tag = tag;
+            
+            if (fread(&entry->bootstrap_method_attr_index, sizeof(entry->bootstrap_method_attr_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->bootstrap_method_attr_index = ntohs(entry->bootstrap_method_attr_index);
+            
+            if (fread(&entry->name_and_type_index, sizeof(entry->name_and_type_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->name_and_type_index = ntohs(entry->name_and_type_index);
+
+            return entry;
+        }
+
+        case CONSTANT_Package: {
+            CONSTANT_Package_info *entry = malloc(sizeof(CONSTANT_Package_info));
+            entry->tag = tag;
+            
+            if (fread(&entry->name_index, sizeof(entry->name_index), 1, file) != 1) {
+                perror("Error while reading constant_pool");
+                return NULL;
+            }
+            entry->name_index = ntohs(entry->name_index);
+            
+            return entry;
+        }
+
+
+    }
 }
--- implementation/constant_pool.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-
-typedef struct {
-    uint8_t tag;
-    uint16_t name_index;
-} CONSTANT_Class_Info;
-
