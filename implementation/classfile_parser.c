d88a3be3f3770300cf009edf28f4d341fe89c0cc
Lucas Saad Rodrigues <lucassaadro@gmail.com>
2026-05-13 14:53:25 -0300

    classfile document

--- implementation/classfile_parser.c
+++ implementation/classfile_parser.c
@@ -4,6 +4,27 @@
 #include <arpa/inet.h>
 
 
+typedef enum cp_tags {
+    CONSTANT_Utf8 = 1,
+    CONSTANT_Integer = 3,
+    CONSTANT_Flaot = 4,
+    CONSTANT_Long = 5,
+    CONSTANT_Double = 6,
+    CONSTANT_Class = 7,
+    CONSTANT_String = 8,
+    CONSTANT_Fieldred = 9,
+    CONSTANT_Methodref = 10,
+    CONSTANT_InterfaceMethodref = 11,
+    CONSTANT_NameAndType = 12, 
+    CONSTANT_MethodHandle = 15,
+    CONSTANT_MethodType = 16,
+    CONSTANT_Dynamic = 17,
+    CONSTANT_InvokeDynamic = 18,
+    CONSTANT_Module = 19,
+    CONSTANT_Package = 20
+} cp_tags;
+
+
 // Constant Pool entry format
 typedef struct {
     uint8_t tag;
---
+++ implementation/constant_pool.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef CONSTANT_Class_info {
+    uint16_t name_index/
+} CONS
