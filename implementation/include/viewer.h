9e692f50023e060717cdbf955963d5d8fdd8f155
DaviHVL <davihvl@gmail.com>
2026-05-19 08:38:07 -0300

    :sparkles: feat: implementação inicial do módulo exibidor

---
+++ implementation/include/viewer.h
@@ -0,0 +1,13 @@
+#ifndef VIEWER_H
+#define VIEWER_H
+
+#include "class_file.h"
+
+// Funções principais de exibição
+void print_general_information(ClassFile *cf);
+//Implementar demais funções de exibição
+
+// Funções auxiliares 
+const char* get_java_version_string(uint16_t major_version);
+
+#endif
