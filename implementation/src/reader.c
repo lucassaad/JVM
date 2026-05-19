4d46fc82b0576428ca4a8df13e2ab563c0f7dc90
Maria Carolina <carolina.burgum@gmail.com>
2026-05-19 14:11:36 -0300

    rw interfaces and interfaces_count

--- implementation/src/reader.c
+++ implementation/src/reader.c
@@ -54,5 +54,54 @@ int read_classfile(ClassFile *cf, FILE *file) {
         if (tag == CONSTANT_Long || tag == CONSTANT_Double) i++; 
     }
 
+    // Read 'access_flags'
+    if (fread(&cf->access_flags, sizeof(cf->access_flags), 1, file) != 1) {
+        perror("Erro ao ler 'access_flags'");
+        return 1;
+    }
+    cf->access_flags = byteswap_u2(cf->access_flags);
+
+    // Read 'this_class'
+    if (fread(&cf->this_class, sizeof(cf->this_class), 1, file) != 1) {
+        perror("Erro ao ler 'this_class'");
+        return 1;
+    }
+    cf->this_class = byteswap_u2(cf->this_class);
+    
+    
+    // Read 'super_class'
+    if (fread(&cf->super_class, sizeof(cf->super_class), 1, file) != 1) {
+        perror("Erro ao ler 'super_class'");
+        return 1;
+    }
+    cf->super_class = byteswap_u2(cf->super_class);
+
+    // Lê o índice e a quantidade de interfaces
+    if (fread(&cf->interfaces_count, sizeof(cf->interfaces_count), 1, file) != 1) {
+        perror("Erro ao ler 'interfaces_count'");
+        return 1;
+    }
+    cf->interfaces_count = byteswap_u2(cf->interfaces_count);
+
+    if (cf->interfaces_count > 0) {
+        // Aloca o vetor com base na quantidade lida
+        cf->interfaces = malloc(sizeof(uint16_t) * cf->interfaces_count);
+        if (cf->interfaces == NULL) {
+            perror("Erro ao alocar memória para interfaces");
+            return 1;
+        }
+
+        // Lê os índices de 2 em 2 bytes
+        for (int i = 0; i < cf->interfaces_count; i++) {
+            if (fread(&cf->interfaces[i], sizeof(uint16_t), 1, file) != 1) {
+                perror("Erro ao ler índice da interface");
+                return 1;
+            }
+            cf->interfaces[i] = byteswap_u2(cf->interfaces[i]); 
+        }
+    } else {
+        // Se a classe não implementar nenhuma interface
+        cf->interfaces = NULL;
+    }
     return 0;
 }
