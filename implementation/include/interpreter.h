1b627584df365981673cc782904e8b8f61f7d140
DaviHVL <davihvl@gmail.com>
2026-06-25 10:43:48 -0300

    :sparkles: feat: adição da lógica inicial do fetch-decode-execute

---
+++ implementation/include/interpreter.h
@@ -0,0 +1,9 @@
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
+#include "jvm_stack.h"
+
+// Função para iniciar o laço de execução Fetch-Decode-Execute
+void execute_engine(JVMStack *stack);
+
+#endif 
